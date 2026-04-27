49feb5772cad2e284e8473daf0e946a3035e6114
chinthakaSenarathna <chironprabath@gmail.com>
2026-04-27 17:34:38 +0530

    added query ???

--- ride-service/src/main/java/com/rideshare/rideservice/repository/RideRepository.java
+++ ride-service/src/main/java/com/rideshare/rideservice/repository/RideRepository.java
@@ -4,6 +4,11 @@ import com.rideshare.rideservice.model.Ride;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.List;
+
 @EnableJpaRepositories
 public interface RideRepository extends JpaRepository<Ride, String> {
+
+    List<Ride> findByRiderIdOrderByCreatedAtDesc(String riderId);
+
 }
