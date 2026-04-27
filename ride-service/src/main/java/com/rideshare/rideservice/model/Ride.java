3a3da640c90b2bf6d13a8996509e335ab6a1c989
chinthakaSenarathna <chironprabath@gmail.com>
2026-04-27 17:37:45 +0530

    done endpoints ???

--- ride-service/src/main/java/com/rideshare/rideservice/model/Ride.java
+++ ride-service/src/main/java/com/rideshare/rideservice/model/Ride.java
@@ -1,10 +1,7 @@
 package com.rideshare.rideservice.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @Table(name = "rides")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Ride {
--- ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
+++ ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
@@ -12,7 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -67,20 +69,78 @@ public class RideService {
     }
 
     public RideResponse getRideById(String rideId) {
+
+        Ride ride = rideRepository.findById(rideId)
+                .orElseThrow(() -> new RuntimeException("Ride with id " + rideId + " not found"));
+
+        return mapToResponse(ride);
+
     }
 
     public List<RideResponse> getRidesByRider(String riderId) {
-        return null;
+
+        return rideRepository.findByRiderIdOrderByCreatedAtDesc(riderId)
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+
     }
 
     public RideResponse startRide(String rideId) {
-        return null;
+
+        Ride ride = rideRepository.findById(rideId)
+                .orElseThrow(() -> new RuntimeException("Ride with id " + rideId + " not found"));
+
+        if(ride.getRideStatus() != RideStatus.ACCEPTED) {
+            throw new RuntimeException("Ride cannot be started. Current status is " + ride.getRideStatus());
+        }
+
+        ride.setRideStatus(RideStatus.RIDE_STARTED);
+        ride.setStartedTime(LocalDateTime.now());
+        rideRepository.save(ride);
+
+        return mapToResponse(ride);
     }
 
     public RideResponse completeRide(String rideId) {
+
+        Ride ride = rideRepository.findById(rideId)
+                .orElseThrow(() -> new RuntimeException("Ride with id " + rideId + " not found"));
+
+        if(ride.getRideStatus() != RideStatus.RIDE_STARTED) {
+            throw new RuntimeException("Ride cannot be completed. Current status is " + ride.getRideStatus());
+        }
+
+        ride.setRideStatus(RideStatus.COMPLETED);
+        ride.setCompleteTime(LocalDateTime.now());
+        ride.setActualFare(ride.getEstimatedFare());
+        rideRepository.save(ride);
+
+        return mapToResponse(ride);
+
     }
 
     public RideResponse cancelRide(String rideId) {
+
+        Ride ride = rideRepository.findById(rideId)
+                .orElseThrow(() -> new RuntimeException("Ride with id " + rideId + " not found"));
+
+        ride.setRideStatus(RideStatus.CANCELLED);
+        rideRepository.save(ride);
+
+        return mapToResponse(ride);
+
+    }
+
+    // This service called by Matching service to update Ride Status ???
+    public void updateRideWithDriver(String rideId, String driverId) {
+        Ride ride = rideRepository.findById(rideId)
+                .orElseThrow(() -> new RuntimeException("Ride with id " + rideId + " not found"));
+
+        ride.setDriverId(driverId);
+        ride.setRideStatus(RideStatus.ACCEPTED);
+        rideRepository.save(ride);
+
     }
 
     private RideResponse mapToResponse(Ride savedRide) {
@@ -103,4 +163,30 @@ public class RideService {
         response.setCompleteTime(savedRide.getCompleteTime());
         return response;
     }
+
+    private double calculateEstimateFare(RideRequest rideRequest) {
+
+        // Simplified Haversine distance calculation
+        double lat1 = Math.toRadians(rideRequest.getPickupLatitude());
+        double lat2 = Math.toRadians(rideRequest.getDropLatitude());
+
+        double lon1 = Math.toRadians(rideRequest.getPickupLongitude());
+        double lon2 = Math.toRadians(rideRequest.getDropLongitude());
+
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+
+        double a = Math.pow(Math.sin(dLat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                + Math.pow(Math.sin(dLon / 2), 2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double distanceKm = 6371 * c;
+
+        // Base fare: 50RS + 12RS. perKm
+        double fare = 50 + (distanceKm * 12);
+        return Math.round(fare * 100) / 100.0;
+
+    }
+
 }
